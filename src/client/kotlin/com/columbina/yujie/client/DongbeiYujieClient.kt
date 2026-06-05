6fdd90730ca7f9f1326f37e147147431c7e1fe00
Castorice1337 <ilovecastoriceforever@gmail.com>
2026-06-06 01:45:20 +0800

    chore: initialize Dongbei Yujie mod

---
+++ src/client/kotlin/com/columbina/yujie/client/DongbeiYujieClient.kt
@@ -0,0 +1,9 @@
+package com.columbina.yujie.client
+
+import net.fabricmc.api.ClientModInitializer
+
+object DongbeiYujieClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/kotlin/com/columbina/yujie/client/DongbeiYujieDataGenerator.kt
@@ -0,0 +1,9 @@
+package com.columbina.yujie.client
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object DongbeiYujieDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
